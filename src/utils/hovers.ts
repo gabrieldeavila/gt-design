8a97b158ef0636b81c94b463130ab1bea789ef99
Gabriel GT <gabrielavila04@protonmail.com>
2022-11-13 21:14:22 -0300

    feat: :sparkles: Add hover options

---
+++ src/utils/hovers.ts
@@ -0,0 +1,12 @@
+import { css } from 'styled-components';
+
+const scaleTransYOpacity = css`
+  &:hover {
+    transform: scale(1.05) translateY(-0.25rem);
+    opacity: 0.8;
+  }
+`;
+
+export default {
+  scaleTransYOpacity
+};
