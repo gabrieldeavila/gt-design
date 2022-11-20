fd80e57491f271e2a2391ba6f00866eaac8763ed
Gabriel GT <gabrielavila04@protonmail.com>
2022-11-19 21:53:27 -0300

    feat: :sparkles: Create Space component

---
+++ src/components/Space/Space.ts
@@ -0,0 +1,57 @@
+import styled, { css } from "styled-components";
+import { space, SpaceProps } from "styled-system";
+import flex from "../../utils/flex";
+
+const customHeight = css`
+  height: ${(props: { height?: string }) => props.height};
+`;
+
+const Flex = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+  flex-direction: column;
+  width: -webkit-fill-available;
+  ${space}
+
+  ${customHeight};
+`;
+
+const FlexCenter = styled(Flex)`
+  ${flex.alignCenter}
+
+  ${space}
+`;
+
+const FlexCenterRow = styled(Flex)`
+  ${flex.alignCenterCol}
+  ${space}
+`;
+
+const FullSpace = styled.div<SpaceProps>`
+  width: -webkit-fill-available;
+  height: fit-content;
+
+  > * {
+    width: 100%;
+    height: 100%;
+  }
+
+  ${customHeight};
+  ${space}
+`;
+
+const Center = styled.div`
+  ${flex.alignCenter}
+
+  ${customHeight};
+  ${space}
+`;
+
+export default {
+  Flex,
+  FlexCenter,
+  FlexCenterRow,
+  FullSpace,
+  Center,
+};
