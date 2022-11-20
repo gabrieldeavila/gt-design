e0b3cf29dda27839988fb2870a9d773dd8fe2f2d
Gabriel GT <gabrielavila04@protonmail.com>
2022-11-19 21:53:15 -0300

    feat: :sparkles: Create pageState context

--- src/context/gt.tsx
+++ src/context/gt.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, memo, useContext, useMemo } from "react";
 import PropTypes from "prop-types";
+import React, { createContext, useContext, useMemo } from "react";
 import { IGTContext } from "./interface";
 
 // create default values for context
--- src/context/interface.ts
+++ src/context/interface.ts
@@ -3,3 +3,15 @@ export interface IGTContext {
   setDarkTheme: React.Dispatch<React.SetStateAction<boolean>>;
   children?: React.ReactNode;
 }
+
+interface IPageStateValues {
+  [key: string]: string;
+}
+
+export interface IGTPageState {
+  errors: string[];
+  setErrors: React.Dispatch<React.SetStateAction<string[]>>;
+  pageState: IPageStateValues;
+  setPageState: React.Dispatch<React.SetStateAction<IPageStateValues>>;
+  children?: React.ReactNode;
+}
