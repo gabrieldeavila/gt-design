92e44b94bc83aa0e2eee0bc88fad9e1adc6a2655
Gabriel GT <gabrielavila04@protonmail.com>
2023-02-07 21:30:48 -0300

    fix: :bug: mask newposition going too far

--- src/hooks/pageState/mask/getRightPosition.ts
+++ src/hooks/pageState/mask/getRightPosition.ts
@@ -27,14 +27,7 @@ function getRightPosition(
 
     isDeleting.current = false;
 
-    // if the new position is 1, then it should not be considered, because he's already in the first position
-    // like in (19) 99999-9999, if the user deletes the 1, it should not go to the 0 index
-    // but keep in the 1 index
-    if (newPosition === 1) {
-      currPosition.current = 1;
-    } else {
-      currPosition.current = newPosition + 1;
-    }
+    currPosition.current = newPosition + 1;
   } else {
     // and here as we are adding, we need to go to the right
     for (let i = currInpPosition; i < maskedValue.length; i++) {
