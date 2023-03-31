2d3764883a352b903258787d11632470ecc198ad
Gabriel GT <gabrielavila04@protonmail.com>
2023-03-31 20:25:54 -0300

    feat: :tada: example with nextjs

---
+++ examples/next-ts/app/login/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { SectionContainer } from "@geavila/gt-design";
+import React from "react";
+
+function d() {
+  return <SectionContainer title="Inputs" subtitle="STORIES.INPUTS.SUBTITLE" />;
+}
+
+export default d;
