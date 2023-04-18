3e4712aa19b2c70f1904befd23e8ee34b3bcef38
Gabriel GT <gabrielavila04@protonmail.com>
2023-04-17 22:08:09 -0300

    feat: :sparkles: add google provider to next auth

--- examples/next-ts/app/[lang]/page.tsx
+++ examples/next-ts/app/[lang]/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
 import LoginBtn from "./login-btn";
+import { GTLoginProviders } from "@geavila/gt-design";
 
 export default function Page() {
-  return <LoginBtn />;
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        {/* <UserInformation data={session.user} /> */}
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <GTLoginProviders
+      onGoogleClick={() => signIn("google", { redirect: false })}
+      onGitHubClick={() => signIn("github", { redirect: false })}
+    />
+  );
+  // return <LoginBtn />;
 }
--- examples/next-ts/pages/api/auth/[...nextauth].ts
+++ examples/next-ts/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,7 +9,12 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-  ],
-} 
 
-export default NextAuth(authOptions)
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
