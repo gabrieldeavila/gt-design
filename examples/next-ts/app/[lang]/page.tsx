16f56c2ddd9a5928356d93817927c4b6d76e9da5
Gabriel GT <gabrielavila04@protonmail.com>
2023-04-24 21:09:03 -0300

    feat: :sparkles: add i18n in next landing page

--- examples/next-ts/app/[lang]/page.tsx
+++ examples/next-ts/app/[lang]/page.tsx
@@ -1,38 +1,36 @@
 "use client";
 
-import { signIn, signOut, useSession } from "next-auth/react";
 import {
-  Button,
   EasyState,
   GTBasicLandingPage,
   GTInput,
-  GTLoginProviders,
   IGTLandingBenefit,
   IGTLandingFeature,
+  IGTLandingNavbar,
   INonNumericMask,
   INumericMask,
   Input,
-  Navbar,
-  Space,
-  Text,
+  SelectionOptions,
   autoUpdateTheme,
 } from "@geavila/gt-design";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React from "react";
 import { Clock, Edit3, Tool } from "react-feather";
-import React, { useCallback } from "react";
 
 export default function Page() {
   const { data: session } = useSession();
 
   return (
     <GTBasicLandingPage
-      logo="GT Design"
-      options={NavbarOptions}
+      navbarOptions={navbarOptions}
       title="GT Design"
-      description="Design smarter, not harder"
-      benefitDescription="A powerful tool for developers to streamline workflow, save time, and enhance design outcomes."
+      description="TEMPLATE.LANDING_PAGE.DESCRIPTION"
+      benefitDescription="TEMPLATE.LANDING_PAGE.BENEFITS.DESCRIPTION"
       benefits={Benefits}
       features={Features}
-      footerDescription="Designed and built in sleepless nights by Gabriel Avila."
+      callToAction={callToAction}
+      footerDescription="TEMPLATE.LANDING_PAGE.FOOTER.DESCRIPTION"
     />
   );
 
@@ -54,108 +52,68 @@ export default function Page() {
   // return <LoginBtn />;
 }
 
-const NavbarOptions = () => {
-  return (
-    <>
-      <Navbar.Options>
-        <Navbar.OptionWrapper>
-          <Space.Modifiers gridGap="1rem">
-            <Text.Action>Docs</Text.Action>
-            <Text.Action>GitHub</Text.Action>
-            <Text.Action>npm</Text.Action>
-          </Space.Modifiers>
-        </Navbar.OptionWrapper>
+const navbarOptions: IGTLandingNavbar = {
+  logo: "GT",
+  button: {
+    description: "TEMPLATE.LANDING_PAGE.LOG_IN",
+    onClick: () => console.log("button"),
+  },
+  options: [
+    { description: "Docs", onClick: () => console.log("docs") },
+    { description: "GitHub", onClick: () => console.log("GitHub") },
+    { description: "npm", onClick: () => console.log("npm") },
+  ],
+};
 
-        <Navbar.OptionWrapper>
-          <Space.Modifiers gridGap="1rem">
-            <Button.Contrast
-              defaultSize="sm"
-              fitContent
-              px="1.25rem"
-              py="0.5rem"
-              borderRadius="2rem"
-            >
-              Log in
-            </Button.Contrast>
-          </Space.Modifiers>
-        </Navbar.OptionWrapper>
-      </Navbar.Options>
-    </>
-  );
+const callToAction = {
+  title: "TEMPLATE.LANDING_PAGE.ACTION.TITLE",
+  description: "TEMPLATE.LANDING_PAGE.ACTION.DESCRIPTION",
+  button: "TEMPLATE.LANDING_PAGE.ACTION.GET_STARTED",
 };
 
 const Benefits: IGTLandingBenefit[] = [
   {
-    title: "Save time",
-    description:
-      "By automating repetitive tasks, you can free up more time to focus on the creative aspects of your work",
+    title: "TEMPLATE.LANDING_PAGE.BENEFITS.SAVE_TIME.TITLE",
+    description: "TEMPLATE.LANDING_PAGE.BENEFITS.SAVE_TIME.DESCRIPTION",
     icon: <Clock />,
   },
   {
-    title: "Improve efficiency",
+    title: "TEMPLATE.LANDING_PAGE.BENEFITS.IMPROVE_EFFICIENCY.TITLE",
     description:
-      "Streamline workflow and reduce manual labor for faster, more accurate project completion without sacrificing quality.",
+      "TEMPLATE.LANDING_PAGE.BENEFITS.IMPROVE_EFFICIENCY.DESCRIPTION",
     icon: <Tool />,
   },
   {
-    title: "Enhance outcomes",
-    description:
-      "From phone masking to email validation, we ensure that your applications look and function exactly as intended.",
+    title: "TEMPLATE.LANDING_PAGE.BENEFITS.ENHANCE_OUTCOMES.TITLE",
+    description: "TEMPLATE.LANDING_PAGE.BENEFITS.ENHANCE_OUTCOMES.DESCRIPTION",
     icon: <Edit3 />,
   },
 ];
 
 const Features: IGTLandingFeature[] = [
   {
-    title: "Save time",
-    description:
-      "With easy-to-use validation for emails, numbers, and more, you can ensure that your data is accurate and reliable.",
+    title: "TEMPLATE.LANDING_PAGE.FEATURES.SAVE_TIME.TITLE",
+    description: "TEMPLATE.LANDING_PAGE.FEATURES.SAVE_TIME.DESCRIPTION",
     component: <ExampleValidation />,
   },
   {
-    title: "Global Reach with i18n",
-    description:
-      "GT Design includes i18n by default, ensuring that your application is fully localized and ready to use in any language.",
-    component: <Clock />,
+    title: "TEMPLATE.LANDING_PAGE.FEATURES.GLOBAL_REACH.TITLE",
+    description: "TEMPLATE.LANDING_PAGE.FEATURES.GLOBAL_REACH.DESCRIPTION",
+    component: <ExampleI18n />,
   },
   {
-    title: "Masking by Default",
+    title: "TEMPLATE.LANDING_PAGE.FEATURES.MASKING_BY_DEFAULT.TITLE",
     description:
-      "GT Design also includes powerful number masking tools for phone numbers and other numeric data, making it easy to display information in the format you want.",
+      "TEMPLATE.LANDING_PAGE.FEATURES.MASKING_BY_DEFAULT.DESCRIPTION",
     component: <ExampleMasking />,
   },
   {
-    title: "Easy-to-style theme",
+    title: "TEMPLATE.LANDING_PAGE.FEATURES.EASY_TO_STYLE_THEME.TITLE",
     description:
-      "You can customize the look and feel of your application to match your brand and user preferences.",
+      "TEMPLATE.LANDING_PAGE.FEATURES.EASY_TO_STYLE_THEME.DESCRIPTION",
     component: <ExampleTheming />,
   },
 ];
-
-function ExampleValidation() {
-  return (
-    <EasyState name="example-validation" initial={{ email: "" }}>
-      <Input.Group>
-        <GTInput.Email
-          title="As easy as it should be"
-          name="email"
-          label="Email"
-        />
-
-        <GTInput.Text
-          title="As easy as it should be"
-          minChars="10"
-          maxChars="20"
-          minWords="2"
-          maxWords="5"
-          name="name"
-          label="TEMPLATE.LOGIN.NAME_LABEL"
-        />
-      </Input.Group>
-    </EasyState>
-  );
-}
-
 function ExampleMasking() {
   const moneyMask: INumericMask = {
     suffix: "",
@@ -179,17 +137,70 @@ function ExampleMasking() {
     <EasyState name="example-masking" initial={{ price: 1 }}>
       <Input.Group>
         <GTInput.Mask
-          title="As easy as it should be"
+          title="EXAMPLE.EASY_AS_SHOULD"
           name="phone"
-          label="Phone"
+          label="EXAMPLE.PHONE"
           mask={phoneMask}
         />
 
         <GTInput.Mask
-          title="As easy as it should be"
+          title="EXAMPLE.EASY_AS_SHOULD"
           name="price"
-          label="Money"
+          label="EXAMPLE.MONEY"
           mask={moneyMask}
+        />
+      </Input.Group>
+    </EasyState>
+  );
+}
+
+function ExampleI18n() {
+  const router = useRouter();
+
+  const options = React.useMemo(
+    () => [
+      { value: "en", label: "🇺🇸 English" },
+      { value: "pt-BR", label: "🇧🇷 Português" },
+    ],
+    []
+  );
+
+  const handleChangeLanguage = React.useCallback((option: SelectionOptions) => {
+    router.push(`/${option.value}`);
+  }, []);
+
+  return (
+    <Input.Group>
+      <EasyState name="example-translations" initial={{ select: "" }}>
+        <GTInput.Select
+          label="TRANSLATE.LANGUAGE"
+          name="select"
+          options={options}
+          onSelect={handleChangeLanguage}
+        />
+      </EasyState>
+    </Input.Group>
+  );
+}
+
+function ExampleValidation() {
+  return (
+    <EasyState name="example-validation" initial={{ email: "" }}>
+      <Input.Group>
+        <GTInput.Email
+          title="EXAMPLE.EASY_AS_SHOULD"
+          name="email"
+          label="Email"
+        />
+
+        <GTInput.Text
+          title="EXAMPLE.EASY_AS_SHOULD"
+          minChars="10"
+          maxChars="20"
+          minWords="2"
+          maxWords="5"
+          name="name"
+          label="TEMPLATE.LOGIN.NAME_LABEL"
         />
       </Input.Group>
     </EasyState>
