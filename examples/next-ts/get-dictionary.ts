bada3214df75f1d34185682e41d68658cb36f2d9
Gabriel GT <gabrielavila04@protonmail.com>
2023-04-29 18:20:32 -0300

    fix: :green_heart: path

--- examples/next-ts/get-dictionary.ts
+++ examples/next-ts/get-dictionary.ts
@@ -9,10 +9,10 @@ interface Dictionary {
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
 const dictionaries: Dictionary = {
-  en: () => import("./dictionaries/en.json").then((module) => module.default),
-  "en-US": () => import("./dictionaries/en.json").then((module) => module.default),
+  en: () => import("@/dictionaries/en.json").then((module) => module.default),
+  "en-US": () => import("@/dictionaries/en.json").then((module) => module.default),
   "pt-BR": () =>
-    import("./dictionaries/pt-BR.json").then((module) => module.default),
+    import("@/dictionaries/pt-BR.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]();
