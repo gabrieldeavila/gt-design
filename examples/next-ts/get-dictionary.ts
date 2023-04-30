1691214bac5bd61161d1b25a162ae01f4ed59e5a
Gabriel GT <gabrielavila04@protonmail.com>
2023-04-29 21:38:24 -0300

    fix: :bug: try to fix locale error

--- examples/next-ts/get-dictionary.ts
+++ examples/next-ts/get-dictionary.ts
@@ -13,7 +13,7 @@ const dictionaries: Dictionary = {
   "en-US": () =>
     import("@/dictionaries/en.json").then((module) => module.default),
   "pt-BR": () =>
-    import("@/dictionaries/pt-BR.json").then((module) => module.default),
+    import("@/dictionaries/pt.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]();
